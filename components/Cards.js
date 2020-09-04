456851cf16852de39ecbe8c0f207177856ae456c
Joon <jong@jongs-mbp.lan>
2020-09-04 10:59:47 -0700

    finished with stretch

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,159 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+axios
+    .get('https://lambda-times-api.herokuapp.com/articles')
+    .then(response => {
+
+        // fetch articles from each topics
+        const bootStrap = []
+        const javascript = []
+        const jquery = []
+        const node = []
+        const technology = []
+
+        response.data.articles.bootstrap.forEach( each => {
+            bootStrap.push(each)
+        })
+        
+        response.data.articles.javascript.forEach( each => {
+            javascript.push(each)
+        })
+        
+        response.data.articles.jquery.forEach( each => {
+            jquery.push(each)
+        })
+        
+        response.data.articles.node.forEach( each => {
+            node.push(each)
+        })
+        
+        response.data.articles.technology.forEach( each => {
+            technology.push(each)
+        })
+
+        //Append info to '.card-container' (for stretch goal: ADDED one more arguments to filter the specific cards)
+        bootStrap.forEach( info => {
+            cardsContainer.appendChild(createArticle(info, 'bootstrap'))
+        })
+
+        javascript.forEach( info => {
+            cardsContainer.appendChild(createArticle(info, 'javascript'))
+        })
+
+        jquery.forEach( info => {
+            cardsContainer.appendChild(createArticle(info, 'jquery'))
+        })
+
+        node.forEach( info => {
+            cardsContainer.appendChild(createArticle(info, 'node'))
+        })
+
+        technology.forEach( info => {
+            cardsContainer.appendChild(createArticle(info, 'technology'))
+        })
+
+        const bs = Array.from(document.querySelectorAll('.bootstrap'))
+        const js = Array.from(document.querySelectorAll('.javascript'))
+        const jq = Array.from(document.querySelectorAll('.jquery'))
+        const nd = Array.from(document.querySelectorAll('.node'))
+        const tn = Array.from(document.querySelectorAll('.technology'))
+
+        const noBS = js.concat(jq, nd, tn)
+        const noJS = bs.concat(jq, nd, tn)
+        const noJQ = bs.concat(js, nd, tn)
+        const noND = bs.concat(js, jq, tn)
+        const noTN = bs.concat(js, jq, nd)
+        
+        document.addEventListener('click', e => {
+            if(e.target.textContent === 'bootstrap'){
+                noBS.forEach(element => {
+                    if(!element.classList.contains('hide')){
+                        element.classList.add('hide')
+                    }
+                })
+                bs.forEach(element => {
+                    element.classList.remove('hide')
+                })
+
+            } else if(e.target.textContent === 'javascript'){
+                noJS.forEach(element => {
+                    if(!element.classList.contains('hide')){
+                        element.classList.add('hide')
+                    }
+                })
+                js.forEach(element => {
+                    element.classList.remove('hide')
+                })
+            } else if(e.target.textContent === 'jquery'){
+                noJQ.forEach(element => {
+                    if(!element.classList.contains('hide')){
+                        element.classList.add('hide')
+                    }
+                })
+                jq.forEach(element => {
+                    element.classList.remove('hide')
+                })
+            } else if(e.target.textContent === 'node.js'){
+                noND.forEach(element => {
+                    if(!element.classList.contains('hide')){
+                        element.classList.add('hide')
+                    }
+                })
+                nd.forEach(element => {
+                    element.classList.remove('hide')
+                })
+            } else if(e.target.textContent === 'technology'){
+                noTN.forEach(element => {
+                    if(!element.classList.contains('hide')){
+                        element.classList.add('hide')
+                    }
+                })
+                tn.forEach(element => {
+                    element.classList.remove('hide')
+                })
+            }
+        })
+    })
+    .catch(error => {
+        console.log('Error: ', error)
+    })
+
+const cardsContainer = document.querySelector('.cards-container')
+
+function createArticle(data, addClass){
+
+    //Create Elements
+    const cardInfo = document.createElement('div')
+    const headLine = document.createElement('div')
+    const authorInfo = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const imgAuthor = document.createElement('img')
+    const nameAuthor = document.createElement('span')
+
+    //Add attributes
+    cardInfo.classList.add('card', addClass)
+    headLine.classList.add('headline')
+    authorInfo.classList.add('author')
+    imgContainer.classList.add('img-container')
+    
+    //ADD info
+    headLine.textContent = data.headline
+    imgAuthor.src = data.authorPhoto
+    nameAuthor.textContent = data.authorName
+
+    //Appending
+    imgContainer.appendChild(imgAuthor)
+    authorInfo.appendChild(imgContainer)
+    authorInfo.appendChild(nameAuthor)
+    cardInfo.appendChild(headLine)
+    cardInfo.appendChild(authorInfo)
+
+    //Add eventListener
+    cardInfo.addEventListener('click', () => {
+        console.log(headLine.textContent)
+    })
+
+    return cardInfo
+}
